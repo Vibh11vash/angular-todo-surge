b38cdf1efedae0f36516a60f9abb39eb6835a558
vibhore <vibhore.bhadanitech@gmail.com>
2021-09-10 13:40:58 +0530

    to-do

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ToDoListComponent  } from './to-do-list/to-do-list.component';
+
+const routes: Routes = [
+  { path: 'to-do-list', component: ToDoListComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
